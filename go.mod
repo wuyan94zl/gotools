527d24dcf6986325e655cd365d78e96626aecb7e
wuyan94zl <466720682@qq.com>
2022-08-19 10:20:35 +0800

    jwt tools

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
---
+++ jwt/jwt.go
@@ -0,0 +1,64 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt"
+	"github.com/spf13/viper"
+	"strings"
+	"time"
+)
+
+type Config struct {
+	Export    int
+	Secretary string
+}
+
+type CustomClaims struct {
+	Data map[string]interface{}
+	jwt.StandardClaims
+}
+
+func GenToken(c Config, data map[string]interface{}) (map[string]interface{}, error) {
+	expTime := time.Now().Add(time.Duration(c.Export) * time.Second).Unix()
+	customClaims := &CustomClaims{
+		Data: data,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expTime,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customClaims)
+	tokenString, err := token.SignedString([]byte(viper.GetString("jwt.secretary")))
+	if err != nil {
+		return nil, err
+	}
+	rlt := make(map[string]interface{})
+	rlt["expTime"] = expTime
+	rlt["token"] = tokenString
+	return rlt, nil
+}
+
+func AuthBearerToken(c Config, tokenString string) (map[string]interface{}, error) {
+	if tokenString == "" {
+		return nil, errors.New("无效 token")
+	}
+	kv := strings.Split(tokenString, " ")
+	if kv[0] != "Bearer" {
+		return nil, errors.New("无效 token")
+	}
+	tokenString = kv[1]
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("认证失败：%v", token.Header["alg"])
+		}
+		return []byte(c.Secretary), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("认证失败：%v", err)
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, errors.New("token 已失效")
+	}
+}
